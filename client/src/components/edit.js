4754890d2f8e457c4226bc7016883f95c5428bf2
J Ben Anderson <revovisions@gmail.com>
2022-04-01 12:30:21 -0400

    deleted src folder contents, added app.js code and rest of front end, --working front & backend

--- client/src/components/edit.js
+++ client/src/components/edit.js
@@ -1,13 +1,43 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router";
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router";
 
-export default function Create() {
+export default function Edit() {
   const [form, setForm] = useState({
     name: "",
     position: "",
     level: "",
+    records: [],
   });
+  const params = useParams();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    async function fetchData() {
+      const id = params.id.toString();
+      const response = await fetch(
+        `http://localhost:5000/record/${params.id.toString()}`
+      );
+
+      if (!response.ok) {
+        const message = `An error has occurred: ${response.statusText}`;
+        window.alert(message);
+        return;
+      }
+
+      const record = await response.json();
+      if (!record) {
+        window.alert(`Record with id ${id} not found`);
+        navigate("/");
+        return;
+      }
+
+      setForm(record);
+    }
+
+    fetchData();
+
+    return;
+  }, [params.id, navigate]);
 
   // These methods will update the state properties.
   function updateForm(value) {
@@ -16,35 +46,33 @@ export default function Create() {
     });
   }
 
-  // This function will handle the submission.
   async function onSubmit(e) {
     e.preventDefault();
+    const editedPerson = {
+      name: form.name,
+      position: form.position,
+      level: form.level,
+    };
 
-    // When a post request is sent to the create url, we'll add a new record to the database.
-    const newPerson = { ...form };
-
-    await fetch("http://localhost:5000/record/add", {
+    // This will send a post request to update the data in the database.
+    await fetch(`http://localhost:5000/update/${params.id}`, {
       method: "POST",
+      body: JSON.stringify(editedPerson),
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(newPerson),
-    }).catch((error) => {
-      window.alert(error);
-      return;
     });
 
-    setForm({ name: "", position: "", level: "" });
     navigate("/");
   }
 
-  // This following section will display the form that takes the input from the user.
+  // This following section will display the form that takes input from the user to update the data.
   return (
     <div>
-      <h3>Create New Record</h3>
+      <h3>Update Record</h3>
       <form onSubmit={onSubmit}>
         <div className="form-group">
-          <label htmlFor="name">Name</label>
+          <label htmlFor="name">Name: </label>
           <input
             type="text"
             className="form-control"
@@ -54,7 +82,7 @@ export default function Create() {
           />
         </div>
         <div className="form-group">
-          <label htmlFor="position">Position</label>
+          <label htmlFor="position">Position: </label>
           <input
             type="text"
             className="form-control"
@@ -107,10 +135,12 @@ export default function Create() {
             </label>
           </div>
         </div>
+        <br />
+
         <div className="form-group">
           <input
             type="submit"
-            value="Create person"
+            value="Update Record"
             className="btn btn-primary"
           />
         </div>
